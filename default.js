d4fdfaa99d212d704202b008c147a6eca122cb09
almamuntmn <almamuncool88@gmail.com>
2025-07-20 09:35:06 +0600

    second

--- default.js
+++ default.js
@@ -1,9 +1,22 @@
-function add(num1, num2) {
+function add(num1 = 88, num2 = 0) {
     const result = num1 + num2;
     console.log(num1, num2, result);
     return result;
 }
 
-const sum = add(5, 7);
+const sum = add();
+
+// or
+
+function fullName(firsName, lastName = '') {
+    const full = firsName + lastName;
+}
+
+console.log('Noboni');
+
 
 // -------------------------------------------------// -------------------------------------------------
+
+// default array = []
+// default object = []
+// default string = ''
---
+++ template.js
@@ -0,0 +1,9 @@
+
+
+// -------------------------------------------------// -------------------------------------------------
+
+const first = 'Jaan'
+const last = 'pakhi'
+
+const name = `My girl Friend Name is ${first} ${last}`
+console.log(name);
